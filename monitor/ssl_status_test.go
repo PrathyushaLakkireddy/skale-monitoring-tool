75d810ac6f53a9606733443c6af47a6af7a15cb9
NagaTulasi <tulasitulip@gmail.com>
2021-08-30 17:20:01 +0530

    testscripts added

---
+++ monitor/ssl_status_test.go
@@ -0,0 +1,25 @@
+package monitor_test
+
+import (
+	"testing"
+
+	"github.com/PrathyushaLakkireddy/skale-monitoring-tool/config"
+	"github.com/PrathyushaLakkireddy/skale-monitoring-tool/monitor"
+)
+
+func TestGetSslStatus(t *testing.T) {
+	cfg, err := config.ReadFromFile()
+	if err != nil {
+		t.Error("Error while reading config : ", err)
+	}
+	res, err := monitor.GetSslStatus(cfg)
+	if err != nil {
+		t.Error("Error while fetching Schain status")
+	}
+	if &res == nil {
+		t.Error("Expected non empty result, but got empyt result: ", res)
+	}
+	if &res != nil {
+		t.Log("Got SSL status: ", res)
+	}
+}
