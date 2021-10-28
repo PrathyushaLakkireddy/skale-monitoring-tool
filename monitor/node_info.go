e4ecc17eaeab8ee32aee230b2dd86908820ff5f9
NagaTulasi <tulasitulip@gmail.com>
2021-10-28 14:56:05 +0530

    WIP

--- monitor/node_info.go
+++ monitor/node_info.go
@@ -1,36 +1,39 @@
 package monitor
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"os/exec"
+	"strings"
 
 	"github.com/PrathyushaLakkireddy/skale-monitoring-tool/config"
+	"github.com/PrathyushaLakkireddy/skale-monitoring-tool/types"
 )
 
 // GetNodeInfo returns Skale Node Info metrics which are
 // 1. Node Name
 // 2. IP and Public IP
 // 3. port and domain name
-// func GetNodeInfo(cfg *config.Config) (types.NodeInfo, error) {
-// 	log.Println("Getting Node Info...")
+func GetNodeInfo(cfg *config.Config) (types.NodeInfo, error) {
+	log.Println("Getting Node Info...")
 
-// 	cmd := exec.Command("sudo", "skale", "node", "info", "-f", "json")
-// 	out, err := cmd.CombinedOutput()
-// 	if err != nil {
-// 		log.Printf("Error while runnig skale validator cli command %v", err)
-// 	}
-// 	s := string(out)
-// 	s = strings.Replace(s, "'", "\"", -1)
-// 	var result types.NodeInfo
-// 	err = json.Unmarshal([]byte(s), &result)
-// 	if err != nil {
-// 		log.Printf("Error:%v", err)
-// 	}
-// 	return result, nil
-
-// }
-func GetNodeInfo(cfg *config.Config) {
+	cmd := exec.Command("sudo", "skale", "node", "info", "-f", "json")
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Printf("Error while runnig skale validator cli command %v", err)
+	}
+	s := string(out)
+	s = strings.Replace(s, "'", "\"", -1)
+	var result types.NodeInfo
+	err = json.Unmarshal([]byte(s), &result)
+	if err != nil {
+		log.Printf("Error:%v", err)
+	}
+	GetNode(cfg)
+	return result, nil
+}
+func GetNode(cfg *config.Config) {
 	log.Println("Getting Node Info...")
 
 	cmd := exec.Command("sudo", "skale", "node", "info")
